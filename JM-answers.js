d19fc08e1596758c89fd9a7b58521062c35bfdd7
jeff-martin1 <55921609+jeff-martin1@users.noreply.github.com>
2019-09-28 13:43:14 -0700

    Create JM-answers.js

---
+++ JM-answers.js
@@ -0,0 +1,23 @@
+//Setting and Swapping
+//Set myNumber to 42. Set myName to your name.
+//Now swap myNumber into myName & vice versa
+
+var myNumber = "42";
+var myName = "Jeff Martin";
+
+if (myNumber == "42")
+{
+    console.log(myName)
+}
+if (myName == "Jeff Martin")
+{
+    console.log(myNumber)
+}
+
+//Print -52 to 1066
+//Print integers from -52 to 1066 using a FOR loop.
+
+for (var num = -52; num < 1067; num = num + 1)
+{
+    console.log(num)
+}
